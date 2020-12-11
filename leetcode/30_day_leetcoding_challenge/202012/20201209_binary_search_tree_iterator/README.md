fd35a3e528f0c1ca466269c9baafdda17625fbb3
Arivoli Tirouvingadame <arivolit@gmail.com>
2020-12-10 21:10:00 -0800

    binary_search_tree_iterator

---
+++ leetcode/30_day_leetcoding_challenge/202012/20201209_binary_search_tree_iterator/README.md
@@ -0,0 +1,3 @@
+**kiranz**
+<br/>
+https://github.com/kiranz/problems/blob/master/python/173%20Binary%20Search%20Tree%20Iterator.py
---
+++ leetcode/30_day_leetcoding_challenge/202012/20201209_binary_search_tree_iterator/binary_search_tree_iterator.py
@@ -0,0 +1,106 @@
+"""
+Title:  Binary Search Tree Iterator
+
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the 
+constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+int next() Moves the pointer to the right, then returns the number at the pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest
+element in the BST.
+
+You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal 
+when next() is called.
+
+ 
+
+Example 1:
+
+               7
+             /  \
+            3    15
+                /  \
+               9    20
+
+
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+ 
+
+Constraints:
+
+1) The number of nodes in the tree is in the range [1, 105].
+2) 0 <= Node.val <= 106
+3) At most 105 calls will be made to hasNext, and next.
+ 
+
+Follow up:
+
+1) Could you implement next() and hasNext() to run in average O(1) time and use O(h) memory, where h is the height of the tree?
+
+"""
+
+from typing import List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.stack = []
+        self._traverse_left(root)
+
+    def next(self) -> int:
+        node = self.stack.pop()
+        self._traverse_left(node.right)
+        return node.val
+
+    def hasNext(self) -> bool:
+        return self.stack
+
+    def _traverse_left(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+
+
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+
+
+if __name__ == "__main__":
+    None
+
