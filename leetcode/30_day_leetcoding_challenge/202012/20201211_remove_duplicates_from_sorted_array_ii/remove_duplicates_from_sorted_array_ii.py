2d56151b2411473c46a3cea2e68d6784b25bc109
Arivoli Tirouvingadame <arivolit@gmail.com>
2020-12-14 08:54:30 -0800

    remove_duplicates_from_sorted_array_ii

---
+++ leetcode/30_day_leetcoding_challenge/202012/20201211_remove_duplicates_from_sorted_array_ii/remove_duplicates_from_sorted_array_ii.py
@@ -0,0 +1,88 @@
+
+"""
+Title:  Remove Duplicates from Sorted Array II
+
+Given a sorted array nums, remove the duplicates in-place such that duplicates
+appeared at most twice and return the new length.
+
+Do not allocate extra space for another array; you must do this by modifying
+the input array in-place with O(1) extra memory.
+
+Clarification:
+
+Confused why the returned value is an integer, but your answer is an array?
+
+Note that the input array is passed in by reference, which means a modification
+to the input array will be known to the caller.
+
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+
+
+Example 1:
+Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3]
+Explanation: Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively. It doesn't matter what you leave beyond the returned length.
+
+
+
+Example 2:
+Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3]
+Explanation: Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively. It doesn't matter what values are set beyond the returned length.
+
+
+
+Constraints:
+
+1) 0 <= nums.length <= 3 * 10^4
+2) -10^4 <= nums[i] <= 10^4
+3) nums is sorted in ascending order.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) < 3:
+            return len(nums)
+
+        slow = 2
+        for fast in range(2, len(nums)):
+            if nums[slow - 2] != nums[fast]:
+                nums[slow] = nums[fast]
+                slow += 1
+
+        for delete_index in range(fast, slow - 1, -1):
+            del nums[delete_index]
+
+        return slow
+
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    test(solution.removeDuplicates([]), 0)
+    test(solution.removeDuplicates([3]), 1)
+    test(solution.removeDuplicates([3,3]), 2)
+    test(solution.removeDuplicates([1,1,1,2,2,3]), 5)
+    test(solution.removeDuplicates([0,0,1,1,1,1,2,3,3]), 7)
+
