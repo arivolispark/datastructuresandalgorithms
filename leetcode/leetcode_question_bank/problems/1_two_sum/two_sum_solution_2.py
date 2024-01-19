dccf11ffbce99856fd289f099467a553ad490c15
arivolispark <arivolispark@gmail.com>
2024-01-19 14:51:10 -0800

    Update two_sum_solution_2.py

--- leetcode/leetcode_question_bank/problems/1_two_sum/two_sum_solution_2.py
+++ leetcode/leetcode_question_bank/problems/1_two_sum/two_sum_solution_2.py
@@ -1,27 +1,33 @@
+from typing import List
 import math
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result = []
         if nums:
+            result = []
             low, high = math.inf, -math.inf
             sorted_nums = sorted(nums)
             start, end = 0, len(sorted_nums) - 1
 
-            while start <= end:
+            while start < end:
                 if sorted_nums[start] + sorted_nums[end] < target:
-                    start = start + 1
+                    start += 1
                 elif sorted_nums[start] + sorted_nums[end] > target:
-                    end = end - 1
+                    end -= 1
                 elif sorted_nums[start] + sorted_nums[end] == target:
                     low = min(low, sorted_nums[start])
                     high = max(high, sorted_nums[end])
                     break
             
             for i in range(len(nums)):
-                if nums[i] == low and i not in result:
+                if nums[i] == low:
                     result.append(i)
+            for i in range(len(nums)):
                 if nums[i] == high and i not in result:
                     result.append(i)
 
-        return result
+            if len(result) == 2:
+                return result
+            else:
+                return [-1, -1]
+        return [-1, -1]
