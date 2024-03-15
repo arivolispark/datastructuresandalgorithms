74a3239abff0024a3dcd78d19543a9bb375b46b8
arivolispark <arivolispark@gmail.com>
2024-03-14 22:06:13 -0700

    Create 506_relative_ranks.py

---
+++ leetcode/leetcode_question_bank/problems/506_relative_ranks/506_relative_ranks.py
@@ -0,0 +1,29 @@
+from heapq import * 
+
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        max_heap = []
+        ranking_map = {}
+        result = []
+
+        if score:
+            for s in score:
+                heappush(max_heap, -s)
+            
+            ranking = 0
+            while max_heap:
+                value = -heappop(max_heap)
+                ranking += 1
+                if ranking == 1:
+                    ranking_map[value] = "Gold Medal"
+                elif ranking == 2:
+                    ranking_map[value] = "Silver Medal"
+                elif ranking == 3:
+                    ranking_map[value] = "Bronze Medal"
+                else:
+                    ranking_map[value] = str(ranking)
+
+            for num in score:
+                result.append(ranking_map[num])
+
+        return result
