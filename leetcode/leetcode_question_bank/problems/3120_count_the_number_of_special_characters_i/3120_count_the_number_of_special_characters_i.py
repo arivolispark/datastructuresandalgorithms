a8427c5abb2dede63cc9fc339c58134f7994197f
arivolispark <arivolispark@gmail.com>
2024-04-21 20:36:24 -0700

    Update 3120_count_the_number_of_special_characters_i.py

--- leetcode/leetcode_question_bank/problems/3120_count_the_number_of_special_characters_i/3120_count_the_number_of_special_characters_i.py
+++ leetcode/leetcode_question_bank/problems/3120_count_the_number_of_special_characters_i/3120_count_the_number_of_special_characters_i.py
@@ -1,21 +1,16 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
         count = 0
-        unique_letters = set()
         special_chars_map = {}
 
         if word:
             for i in range(len(word)):
-                unique_letters.add(word[i])
+                if word[i] not in special_chars_map:
+                    special_chars_map[word[i]] = word[i]
 
-            for i in unique_letters:
-                if ord(i) >= 97 and ord(i) <= 122:
-                    if i not in special_chars_map:
-                        special_chars_map[i] = i
-            
-            for i in unique_letters:
-                if ord(i) >= 65 and ord(i) <= 90:
-                    if chr(ord(i) + 32) in special_chars_map:
+            for c in special_chars_map.keys():
+                if ord(c) >= 97 and ord(c) <= 122:
+                    if chr(ord(c) - 32) in special_chars_map:
                         count += 1
 
         return count
