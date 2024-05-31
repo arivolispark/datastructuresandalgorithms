079a142e3fe9f761ee451caf5b47212cfa69e450
arivolispark <arivolispark@gmail.com>
2024-05-30 18:02:18 -0700

    Create 260_single_number_iii.py

---
+++ leetcode/leetcode_question_bank/problems/260_single_number_iii/260_single_number_iii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        result = []
+
+        map = {}
+        for num in nums:
+            if not num in map:
+                map[num] = num
+            else:
+                del map[num]
+
+        for key in map.keys():
+            result.append(key)
+
+        return result
