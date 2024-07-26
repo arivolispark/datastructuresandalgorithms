bfcfd47fa56985736bfb92efe1b5b4179e342de6
arivolispark <arivolispark@gmail.com>
2024-07-26 12:46:18 -0700

    Update 2191_sort_the_jumbled_numbers.py

--- leetcode/leetcode_question_bank/problems/2191_sort_the_jumbled_numbers/2191_sort_the_jumbled_numbers.py
+++ leetcode/leetcode_question_bank/problems/2191_sort_the_jumbled_numbers/2191_sort_the_jumbled_numbers.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        def getMapped(num: int) -> int:
+            mapped_num = []
+            for ch in str(num):
+                mapped_num.append(str(mapping[ord(ch) - ord('0')]))
+            return int(''.join(mapped_num))
+        result = [(getMapped(num), i, num) for i, num in enumerate(nums)]
+        return [num for _, i, num in sorted(result)]
