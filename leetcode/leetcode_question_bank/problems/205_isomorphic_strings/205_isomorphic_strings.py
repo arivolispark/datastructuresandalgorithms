947cd9a0a7c74c2c47e7b0742619b38188745e1f
arivolispark <arivolispark@gmail.com>
2024-07-28 17:03:55 -0700

    Update 205_isomorphic_strings.py

--- leetcode/leetcode_question_bank/problems/205_isomorphic_strings/205_isomorphic_strings.py
+++ leetcode/leetcode_question_bank/problems/205_isomorphic_strings/205_isomorphic_strings.py
@@ -2,18 +2,17 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         map = {}
         s_len, t_len = len(s), len(t)
-        
+
         if s_len != t_len:
             return False
 
         for i in range(s_len):
             if s[i] in map:
-                v = map[s[i]]
-                if t[i] != v:
+                if t[i] != map[s[i]]:
                     return False
             else:
                 if t[i] in map.values():
                     return False
                 map[s[i]] = t[i]
-
+                
         return True
