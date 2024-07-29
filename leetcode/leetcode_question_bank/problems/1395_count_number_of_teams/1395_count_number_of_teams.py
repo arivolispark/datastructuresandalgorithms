a292bd90f784cb67922b3f137ac7a86d2b28822c
arivolispark <arivolispark@gmail.com>
2024-07-29 14:12:11 -0700

    Update 1395_count_number_of_teams.py

--- leetcode/leetcode_question_bank/problems/1395_count_number_of_teams/1395_count_number_of_teams.py
+++ leetcode/leetcode_question_bank/problems/1395_count_number_of_teams/1395_count_number_of_teams.py
@@ -1,1 +1,23 @@
-
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        length = len(rating)
+        result = 0
+        
+        for i in range(length):
+            less_before, greater_before = 0, 0
+            less_after, greater_after = 0, 0
+            
+            for j in range(i):
+                if rating[j] < rating[i]:
+                    less_before += 1
+                elif rating[j] > rating[i]:
+                    greater_before += 1
+            
+            for j in range(i + 1, length):
+                if rating[j] < rating[i]:
+                    less_after += 1
+                elif rating[j] > rating[i]:
+                    greater_after += 1
+            
+            result += less_before * greater_after + greater_before * less_after
+        return result
