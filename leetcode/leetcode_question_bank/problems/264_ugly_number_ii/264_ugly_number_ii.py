912f1fd5b77412572ba720fdecfebe1e73d3148c
arivolispark <arivolispark@gmail.com>
2024-08-17 19:33:08 -0700

    Update 264_ugly_number_ii.py

--- leetcode/leetcode_question_bank/problems/264_ugly_number_ii/264_ugly_number_ii.py
+++ leetcode/leetcode_question_bank/problems/264_ugly_number_ii/264_ugly_number_ii.py
@@ -24,18 +24,16 @@ Constraints:
 
 """
 from heapq import *
-import math
-
 
 class Solution:
 
     def nthUglyNumber(self, n: int) -> int:
+        if n == 1:
+            return 1
+
         min_heap = []
         s = set()
         count = 0
-
-        if n == 1:
-            return 1
 
         if 1 not in s:
             heappush(min_heap, 1)
@@ -45,12 +43,12 @@ class Solution:
             count += 1
             val = heappop(min_heap)
 
+            if count == n:
+                return val
+
             val_mul_2 = val * 2
             val_mul_3 = val * 3
             val_mul_5 = val * 5
-
-            if count == n:
-                return val
 
             if val_mul_2 not in s:
                 heappush(min_heap, val * 2)
