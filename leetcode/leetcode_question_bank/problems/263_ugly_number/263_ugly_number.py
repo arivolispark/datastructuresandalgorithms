34ffffc6ff018ab74c7c241660cad810d5afd555
arivolispark <arivolispark@gmail.com>
2024-08-25 13:50:37 -0700

    Update 263_ugly_number.py

--- leetcode/leetcode_question_bank/problems/263_ugly_number/263_ugly_number.py
+++ leetcode/leetcode_question_bank/problems/263_ugly_number/263_ugly_number.py
@@ -1,1 +1,12 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        prime_factor = [2, 3, 5]
 
+        if n == 0:
+            return False
+
+        for a in prime_factor:
+            while n % a == 0:
+                n //= a
+
+        return n == 1
