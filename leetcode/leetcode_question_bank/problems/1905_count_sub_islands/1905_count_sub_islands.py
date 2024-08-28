c36d484b161c76d52b3999ecc6e306090bf143be
arivolispark <arivolispark@gmail.com>
2024-08-27 20:59:31 -0700

    Update 1905_count_sub_islands.py

--- leetcode/leetcode_question_bank/problems/1905_count_sub_islands/1905_count_sub_islands.py
+++ leetcode/leetcode_question_bank/problems/1905_count_sub_islands/1905_count_sub_islands.py
@@ -1,1 +1,75 @@
+"""
+Title:  1905. Count Sub Islands
 
+You are given two m x n binary matrices grid1 and grid2 containing only 0's (representing water)
+and 1's (representing land). An island is a group of 1's connected 4-directionally (horizontal
+or vertical). Any cells outside of the grid are considered water cells.
+
+An island in grid2 is considered a sub-island if there is an island in grid1 that contains all
+the cells that make up this island in grid2.
+
+Return the number of islands in grid2 that are considered sub-islands.
+
+
+
+Example 1:
+Input: grid1 = [[1,1,1,0,0],[0,1,1,1,1],[0,0,0,0,0],[1,0,0,0,0],[1,1,0,1,1]], grid2 = [[1,1,1,0,0],[0,0,1,1,1],[0,1,0,0,0],[1,0,1,1,0],[0,1,0,1,0]]
+Output: 3
+Explanation: In the picture above, the grid on the left is grid1 and the grid on the right is grid2.
+The 1s colored red in grid2 are those considered to be part of a sub-island. There are three sub-islands.
+
+
+Example 2:
+Input: grid1 = [[1,0,1,0,1],[1,1,1,1,1],[0,0,0,0,0],[1,1,1,1,1],[1,0,1,0,1]], grid2 = [[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]]
+Output: 2
+Explanation: In the picture above, the grid on the left is grid1 and the grid on the right is grid2.
+The 1s colored red in grid2 are those considered to be part of a sub-island. There are two sub-islands.
+
+
+Constraints:
+1) m == grid1.length == grid2.length
+2) n == grid1[i].length == grid2[i].length
+3) 1 <= m, n <= 500
+4) grid1[i][j] and grid2[i][j] are either 0 or 1.
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        answer = 0
+        rows = len(grid2)
+        columns = len(grid2[0])
+
+        def dfs(i: int, j: int):
+            if i < 0 or i == rows or j < 0 or j == columns:
+                return 1
+            if grid2[i][j] != 1:
+                return 1
+
+            grid2[i][j] = 2  # visited
+            return (dfs(i + 1, j) & dfs(i - 1, j) & dfs(i, j + 1) & dfs(i, j - 1) & grid1[i][j])
+
+        for i in range(rows):
+            for j in range(columns):
+                if grid2[i][j] == 1:
+                    answer += dfs(i, j)
+
+        return answer
+
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    test(solution.countSubIslands([[1,1,1,0,0],[0,1,1,1,1],[0,0,0,0,0],[1,0,0,0,0],[1,1,0,1,1]], [[1,1,1,0,0],[0,0,1,1,1],[0,1,0,0,0],[1,0,1,1,0],[0,1,0,1,0]]), 3)
+    test(solution.countSubIslands([[1,0,1,0,1],[1,1,1,1,1],[0,0,0,0,0],[1,1,1,1,1],[1,0,1,0,1]], [[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]]), 2)
