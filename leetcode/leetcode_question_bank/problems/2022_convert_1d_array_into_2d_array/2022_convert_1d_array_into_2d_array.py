5619f998f49b3d46a43606bc97f5ffd834236181
arivolispark <arivolispark@gmail.com>
2024-08-31 23:53:21 -0700

    Update 2022_convert_1d_array_into_2d_array.py

--- leetcode/leetcode_question_bank/problems/2022_convert_1d_array_into_2d_array/2022_convert_1d_array_into_2d_array.py
+++ leetcode/leetcode_question_bank/problems/2022_convert_1d_array_into_2d_array/2022_convert_1d_array_into_2d_array.py
@@ -1,1 +1,19 @@
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if m * n != len(original):
+            return [[]]
 
+        answer = []
+        i, j = 0, 0
+
+        while i < m:
+            row = []
+            while j < n:
+                row.append(original[j])
+                j += 1
+                if j == n:
+                    answer.append(row)
+                    j = 0
+                    break
+            i += 1
+        return answer
