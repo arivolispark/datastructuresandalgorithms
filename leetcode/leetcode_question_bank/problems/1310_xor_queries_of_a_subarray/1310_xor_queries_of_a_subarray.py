c1f403e03a32b2e5479d7a4aacf5228ab1b7b2ae
arivolispark <arivolispark@gmail.com>
2024-09-12 22:39:51 -0700

    Update 1310_xor_queries_of_a_subarray.py

--- leetcode/leetcode_question_bank/problems/1310_xor_queries_of_a_subarray/1310_xor_queries_of_a_subarray.py
+++ leetcode/leetcode_question_bank/problems/1310_xor_queries_of_a_subarray/1310_xor_queries_of_a_subarray.py
@@ -1,7 +1,9 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        l = len(arr)
-        prexor = [0] * (l+1)
-        for i in range(1, l+1):
+        length = len(arr)
+        prexor = [0] * (length + 1)
+        
+        for i in range(1, length + 1):
             prexor[i] ^= prexor[i-1] ^ arr[i-1]
+        
         return [prexor[j+1] ^ prexor[i] for i, j in queries]
