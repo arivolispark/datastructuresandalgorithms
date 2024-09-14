81c71978d2b8a668e6333fbcf1c5bb0c5b67db42
arivolispark <arivolispark@gmail.com>
2024-09-14 15:37:12 -0700

    Update 2419_longest_subarray_with_maximum_bitwise_AND_solution_1.py

--- leetcode/leetcode_question_bank/problems/2419_longest_subarray_with_maximum_bitwise_AND/2419_longest_subarray_with_maximum_bitwise_AND_solution_1.py
+++ leetcode/leetcode_question_bank/problems/2419_longest_subarray_with_maximum_bitwise_AND/2419_longest_subarray_with_maximum_bitwise_AND_solution_1.py
@@ -1,1 +1,18 @@
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        max_val = nums[0]
+        curr_len = 1
+        max_len = 1
 
+        for i in range(1, len(nums)):
+            if nums[i] > max_val:
+                max_val = nums[i]
+                curr_len = 1
+                max_len = 1
+            elif nums[i] == max_val:
+                curr_len += 1
+                max_len = max(curr_len, max_len)
+            else:
+                curr_len = 0
+
+        return max_len
