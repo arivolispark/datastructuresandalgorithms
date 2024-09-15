037a7d61f54888b5fe72de7e5c35ee27db284b2f
arivolispark <arivolispark@gmail.com>
2024-09-15 14:49:25 -0700

    Update 1371_find_the_longest_substring_containing_vowels_in_even_counts.py

--- leetcode/leetcode_question_bank/problems/1371_find_the_longest_substring_containing_vowels_in_even_counts/1371_find_the_longest_substring_containing_vowels_in_even_counts.py
+++ leetcode/leetcode_question_bank/problems/1371_find_the_longest_substring_containing_vowels_in_even_counts/1371_find_the_longest_substring_containing_vowels_in_even_counts.py
@@ -1,15 +1,15 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
         answer = 0
-        kVowels = 'aeiou'
+        vowels = "aeiou"
         prefix = 0  # the binary prefix
-        prefixToIndex = {0: -1}
+        prefix_to_index = {0: -1}
 
         for i, c in enumerate(s):
-            index = kVowels.find(c)
+            index = vowels.find(c)
             if index != -1:
                 prefix ^= 1 << index
-            prefixToIndex.setdefault(prefix, i)
-            answer = max(answer, i - prefixToIndex[prefix])
+            prefix_to_index.setdefault(prefix, i)
+            answer = max(answer, i - prefix_to_index[prefix])
 
         return answer
