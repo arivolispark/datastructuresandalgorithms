914fd4a17338b1b6c197a23436f8d68415bac553
arivolispark <arivolispark@gmail.com>
2024-09-17 23:48:27 -0700

    Update 179_largest_number.py

--- leetcode/leetcode_question_bank/problems/179_largest_number/179_largest_number.py
+++ leetcode/leetcode_question_bank/problems/179_largest_number/179_largest_number.py
@@ -1,1 +1,18 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums_as_str = [str(num) for num in nums]
+      
+        nums_as_str.sort(key=cmp_to_key(self._compare_numbers))
+      
+        # Special case: if the highest number is "0", the result is "0"
+        # (happens when all numbers are zeros)
+        if nums_as_str[0] == "0":
+            return "0"
+      
+        return "".join(nums_as_str)
 
+    def _compare_numbers(self, a: str, b: str) -> int:
+        if a + b < b + a:
+            return 1
+        else:
+            return -1
