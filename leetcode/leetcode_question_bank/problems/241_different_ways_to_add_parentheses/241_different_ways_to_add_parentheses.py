cdb9e1ea6287ed42fca3eae401933513439468f0
arivolispark <arivolispark@gmail.com>
2024-09-18 19:27:20 -0700

    Update 241_different_ways_to_add_parentheses.py

--- leetcode/leetcode_question_bank/problems/241_different_ways_to_add_parentheses/241_different_ways_to_add_parentheses.py
+++ leetcode/leetcode_question_bank/problems/241_different_ways_to_add_parentheses/241_different_ways_to_add_parentheses.py
@@ -1,1 +1,19 @@
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        if expression.isdigit():
+            return [int(expression)]
 
+        result = []
+        for i in range(len(expression)):
+            if expression[i] in "+-*":
+                left = self.diffWaysToCompute(expression[:i])
+                right = self.diffWaysToCompute(expression[i+1:])
+                for l in left:
+                    for r in right:
+                        if expression[i] == '+':
+                            result.append(l+r)
+                        elif expression[i] == '-':
+                            result.append(l-r)
+                        elif expression[i] == '*':
+                            result.append(l*r)
+        return result
