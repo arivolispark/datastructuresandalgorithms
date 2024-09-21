ab76cccf8b7c4b65c10bc0c89978ecbee0ee7b0e
arivolispark <arivolispark@gmail.com>
2024-09-21 16:25:43 -0700

    Update 386_lexicographical_numbers.py

--- leetcode/leetcode_question_bank/problems/386_lexicographical_numbers/386_lexicographical_numbers.py
+++ leetcode/leetcode_question_bank/problems/386_lexicographical_numbers/386_lexicographical_numbers.py
@@ -1,1 +1,15 @@
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        result = []
+        curr = 1
 
+        while len(result) < n:
+            result.append(curr)
+            if curr * 10 <= n:
+                curr *= 10
+            else:
+                while curr % 10 == 9 or curr == n:
+                    curr //= 10
+                curr += 1
+
+        return result
