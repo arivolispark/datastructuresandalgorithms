df0aa0119a009643d1dade4e19dc4ecf305f6369
arivolispark <arivolispark@gmail.com>
2024-10-02 13:56:23 -0700

    Update 1331_rank_transform_of_an_array.py

--- leetcode/leetcode_question_bank/problems/1331_rank_transform_of_an_array/1331_rank_transform_of_an_array.py
+++ leetcode/leetcode_question_bank/problems/1331_rank_transform_of_an_array/1331_rank_transform_of_an_array.py
@@ -1,1 +1,21 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        temp, result = [], []
+        map = {}
+        length = len(arr)
 
+        for i in range(length):
+            temp.append(arr[i])
+
+        temp.sort()
+
+        rank = 1
+        for i in range(length):
+            if temp[i] not in map:
+                map[temp[i]] = rank
+                rank += 1
+
+        for i in range(length):
+            result.append(map[arr[i]])
+
+        return result
