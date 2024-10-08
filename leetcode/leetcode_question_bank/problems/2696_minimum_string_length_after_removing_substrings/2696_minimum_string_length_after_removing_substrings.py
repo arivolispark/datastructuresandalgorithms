3b228811564fa7c92cf12cd92b16cb7c0851ccd3
arivolispark <arivolispark@gmail.com>
2024-10-07 17:37:49 -0700

    Update 2696_minimum_string_length_after_removing_substrings.py

--- leetcode/leetcode_question_bank/problems/2696_minimum_string_length_after_removing_substrings/2696_minimum_string_length_after_removing_substrings.py
+++ leetcode/leetcode_question_bank/problems/2696_minimum_string_length_after_removing_substrings/2696_minimum_string_length_after_removing_substrings.py
@@ -1,1 +1,68 @@
+"""
+Title:  2696. Minimum String Length After Removing Substrings
 
+You are given a string s consisting only of uppercase English letters.
+
+You can apply some operations to this string where, in one operation, you can
+remove any occurrence of one of the substrings "AB" or "CD" from s.
+
+Return the minimum possible length of the resulting string that you can obtain.
+
+Note that the string concatenates after removing the substring and could produce
+new "AB" or "CD" substrings.
+
+
+
+Example 1:
+Input: s = "ABFCACDB"
+Output: 2
+Explanation: We can do the following operations:
+- Remove the substring "ABFCACDB", so s = "FCACDB".
+- Remove the substring "FCACDB", so s = "FCAB".
+- Remove the substring "FCAB", so s = "FC".
+So the resulting length of the string is 2.
+It can be shown that it is the minimum length that we can obtain.
+
+
+Example 2:
+Input: s = "ACBBD"
+Output: 5
+Explanation: We cannot do any operations on the string so the length remains the same.
+
+
+
+Constraints:
+1) 1 <= s.length <= 100
+2) s consists only of uppercase English letters.
+
+"""
+
+class Solution:
+
+    def minLength(self, s: str) -> int:
+        stack = []
+
+        for c in s:
+            if c == 'B' and stack and stack[-1] == 'A':
+                stack.pop()
+            elif c == 'D' and stack and stack[-1] == 'C':
+                stack.pop()
+            else:
+                stack.append(c)
+
+        return len(stack)
+
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    test(solution.minLength("ABFCACDB"), 2)
+    test(solution.minLength("ACBBD"), 5)
