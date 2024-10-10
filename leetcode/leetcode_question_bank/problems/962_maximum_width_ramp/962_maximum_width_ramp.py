32bfbd5cc1583ed4ed6b73cbf366a57feb55568d
arivolispark <arivolispark@gmail.com>
2024-10-10 15:13:54 -0700

    Update 962_maximum_width_ramp.py

--- leetcode/leetcode_question_bank/problems/962_maximum_width_ramp/962_maximum_width_ramp.py
+++ leetcode/leetcode_question_bank/problems/962_maximum_width_ramp/962_maximum_width_ramp.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        stack = []
+      
+        for index, value in enumerate(nums):
+            if not stack or nums[stack[-1]] > value:
+                stack.append(index)
+              
+        max_width = 0
+      
+        for i in range(len(nums) - 1, -1, -1):
+            while stack and nums[stack[-1]] <= nums[i]:
+                max_width = max(max_width, i - stack.pop())
+            if not stack:
+                break
+              
+        return max_width
