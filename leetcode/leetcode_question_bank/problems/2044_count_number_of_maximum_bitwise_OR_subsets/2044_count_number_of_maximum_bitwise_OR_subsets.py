acee3ef650f9f8ce7a6692aee978103f3bcef8d6
arivolispark <arivolispark@gmail.com>
2024-10-18 23:43:20 -0700

    Update 2044_count_number_of_maximum_bitwise_OR_subsets.py

--- leetcode/leetcode_question_bank/problems/2044_count_number_of_maximum_bitwise_OR_subsets/2044_count_number_of_maximum_bitwise_OR_subsets.py
+++ leetcode/leetcode_question_bank/problems/2044_count_number_of_maximum_bitwise_OR_subsets/2044_count_number_of_maximum_bitwise_OR_subsets.py
@@ -1,1 +1,17 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        mx = result = 0
+        for x in nums:
+            mx |= x
 
+        def dfs(i, t):
+            nonlocal mx, result
+            if i == len(nums):
+                if t == mx:
+                    result += 1
+                return
+            dfs(i + 1, t)
+            dfs(i + 1, t | nums[i])
+
+        dfs(0, 0)
+        return result
