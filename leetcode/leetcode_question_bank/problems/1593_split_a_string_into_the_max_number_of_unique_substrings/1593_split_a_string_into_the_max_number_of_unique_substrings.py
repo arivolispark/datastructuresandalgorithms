7bf2dbd71894b9824fc8ab5bbd98e0acfaea73e1
arivolispark <arivolispark@gmail.com>
2024-10-21 20:36:34 -0700

    Update 1593_split_a_string_into_the_max_number_of_unique_substrings.py

--- leetcode/leetcode_question_bank/problems/1593_split_a_string_into_the_max_number_of_unique_substrings/1593_split_a_string_into_the_max_number_of_unique_substrings.py
+++ leetcode/leetcode_question_bank/problems/1593_split_a_string_into_the_max_number_of_unique_substrings/1593_split_a_string_into_the_max_number_of_unique_substrings.py
@@ -1,1 +1,17 @@
+class Solution:
+    def maxUniqueSplit(self, s: str) -> int:
+        def dfs(i, t):
+            if i >= len(s):
+                nonlocal result
+                result = max(result, t)
+                return
+            for j in range(i + 1, len(s) + 1):
+                if s[i:j] not in vis:
+                    vis.add(s[i:j])
+                    dfs(j, t + 1)
+                    vis.remove(s[i:j])
 
+        vis = set()
+        result = 1
+        dfs(0, 0)
+        return result
