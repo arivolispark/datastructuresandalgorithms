f978fe30c5a923a0ad178f6acc4a4e87f4e7cc3f
arivolispark <arivolispark@gmail.com>
2024-11-06 15:55:30 -0800

    Update 3011_find_if_array_can_be_sorted.py

--- leetcode/leetcode_question_bank/problems/3011_find_if_array_can_be_sorted/3011_find_if_array_can_be_sorted.py
+++ leetcode/leetcode_question_bank/problems/3011_find_if_array_can_be_sorted/3011_find_if_array_can_be_sorted.py
@@ -1,1 +1,19 @@
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        prevSetBits = 0
+        prevMax, currMax, currMin = -math.inf, -math.inf, math.inf
+        
+        for num in nums:
+            setBits = num.bit_count()
+            if setBits != prevSetBits:
+                if prevMax > currMin:
+                    return False
+                prevSetBits = setBits
+                prevMax = currMax
+                currMax = num
+                currMin = num
+            else:
+                currMax = max(currMax, num)
+                currMin = min(currMin, num)
 
+        return prevMax <= currMin
