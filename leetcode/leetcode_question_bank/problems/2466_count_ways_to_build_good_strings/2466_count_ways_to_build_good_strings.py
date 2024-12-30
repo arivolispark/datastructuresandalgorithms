5aa55d5d7ad7ba78f55aaedfac951dd7cc818fb0
arivolispark <arivolispark@gmail.com>
2024-12-29 21:42:18 -1000

    Update 2466_count_ways_to_build_good_strings.py

--- leetcode/leetcode_question_bank/problems/2466_count_ways_to_build_good_strings/2466_count_ways_to_build_good_strings.py
+++ leetcode/leetcode_question_bank/problems/2466_count_ways_to_build_good_strings/2466_count_ways_to_build_good_strings.py
@@ -1,1 +1,15 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        result = 0
+        kMod = 1_000_000_007
+        dp = [1] + [0] * high
 
+        for i in range(1, high + 1):
+            if i >= zero:
+                dp[i] = (dp[i] + dp[i - zero]) % kMod
+            if i >= one:
+                dp[i] = (dp[i] + dp[i - one]) % kMod
+            if i >= low:
+                result = (result + dp[i]) % kMod
+
+        return result
