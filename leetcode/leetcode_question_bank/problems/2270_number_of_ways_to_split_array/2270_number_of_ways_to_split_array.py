484980fc1d434b3dc90e5d03a9fb0ce37135fead
arivolispark <arivolispark@gmail.com>
2025-01-03 14:53:29 -0800

    Update 2270_number_of_ways_to_split_array.py

--- leetcode/leetcode_question_bank/problems/2270_number_of_ways_to_split_array/2270_number_of_ways_to_split_array.py
+++ leetcode/leetcode_question_bank/problems/2270_number_of_ways_to_split_array/2270_number_of_ways_to_split_array.py
@@ -1,1 +1,19 @@
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        result = 0
+        total = sum(nums)
 
+        progressive_sum_list = []
+        progressive_sum = nums[0]
+
+        progressive_sum_list.append(progressive_sum)        
+
+        for i in range(1, len(nums) - 1):
+            progressive_sum += nums[i]
+            progressive_sum_list.append(progressive_sum)
+
+        for i in range(len(progressive_sum_list)):
+            if progressive_sum_list[i] >= total - progressive_sum_list[i]:
+                result += 1
+             
+        return result
