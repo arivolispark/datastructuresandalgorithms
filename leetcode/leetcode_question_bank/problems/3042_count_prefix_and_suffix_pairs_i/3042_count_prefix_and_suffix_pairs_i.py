952b322c6af9f24494e584b94deae573db330997
arivolispark <arivolispark@gmail.com>
2025-01-07 20:31:04 -0800

    Update 3042_count_prefix_and_suffix_pairs_i.py

--- leetcode/leetcode_question_bank/problems/3042_count_prefix_and_suffix_pairs_i/3042_count_prefix_and_suffix_pairs_i.py
+++ leetcode/leetcode_question_bank/problems/3042_count_prefix_and_suffix_pairs_i/3042_count_prefix_and_suffix_pairs_i.py
@@ -1,1 +1,84 @@
+"""
+Title:
 
+You are given a 0-indexed string array words.
+
+Let's define a boolean function isPrefixAndSuffix that takes two strings, str1 and str2:
+
+- isPrefixAndSuffix(str1, str2) returns true if str1 is both a prefix and a suffix of str2, and false otherwise.
+
+For example, isPrefixAndSuffix("aba", "ababa") is true because "aba" is a prefix
+of "ababa" and also a suffix, but isPrefixAndSuffix("abc", "abcd") is false.
+
+Return an integer denoting the number of index pairs (i, j) such that i < j, and
+isPrefixAndSuffix(words[i], words[j]) is true.
+
+
+
+Example 1:
+Input: words = ["a","aba","ababa","aa"]
+Output: 4
+Explanation: In this example, the counted index pairs are:
+i = 0 and j = 1 because isPrefixAndSuffix("a", "aba") is true.
+i = 0 and j = 2 because isPrefixAndSuffix("a", "ababa") is true.
+i = 0 and j = 3 because isPrefixAndSuffix("a", "aa") is true.
+i = 1 and j = 2 because isPrefixAndSuffix("aba", "ababa") is true.
+Therefore, the answer is 4.
+
+
+Example 2:
+Input: words = ["pa","papa","ma","mama"]
+Output: 2
+Explanation: In this example, the counted index pairs are:
+i = 0 and j = 1 because isPrefixAndSuffix("pa", "papa") is true.
+i = 2 and j = 3 because isPrefixAndSuffix("ma", "mama") is true.
+Therefore, the answer is 2.
+
+
+Example 3:
+Input: words = ["abab","ab"]
+Output: 0
+Explanation: In this example, the only valid index pair is i = 0 and j = 1, and isPrefixAndSuffix("abab", "ab") is false.
+Therefore, the answer is 0.
+
+
+
+Constraints:
+1) 1 <= words.length <= 50
+2) 1 <= words[i].length <= 10
+3) words[i] consists only of lowercase English letters.
+
+"""
+
+from typing import List
+
+class Solution:
+
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        count = 0
+        length = len(words)
+        for i in range(length):
+            for j in range(i+1, length):
+                if isPrefixAndSuffix(words[i], words[j]):
+                    count += 1
+        return count
+
+
+def isPrefixAndSuffix(str1: str, str2: str) -> bool:
+    return True if str2.startswith(str1) and str2.endswith(str1) else False
+
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    test(solution.countPrefixSuffixPairs(["a","aba","ababa","aa"]), 4)
+    test(solution.countPrefixSuffixPairs(["pa","papa","ma","mama"]), 2)
+    test(solution.countPrefixSuffixPairs(["abab","ab"]), 0)
