b7d7eec0a08d5b643f9ca32c4bd663b71f2e51f0
arivolispark <arivolispark@gmail.com>
2025-01-12 16:55:04 -0800

    Update 3223_minimum_length_of_string_after_operations.py

--- leetcode/leetcode_question_bank/problems/3223_minimum_length_of_string_after_operations/3223_minimum_length_of_string_after_operations.py
+++ leetcode/leetcode_question_bank/problems/3223_minimum_length_of_string_after_operations/3223_minimum_length_of_string_after_operations.py
@@ -1,1 +1,16 @@
+from collections import Counter
 
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        result = 0
+        counter = Counter(s)
+        
+        for k, v in counter.items():
+            if v <= 2:
+                result += v
+            elif v & 1:
+                result += 1
+            else:
+                result += 2
+
+        return result
