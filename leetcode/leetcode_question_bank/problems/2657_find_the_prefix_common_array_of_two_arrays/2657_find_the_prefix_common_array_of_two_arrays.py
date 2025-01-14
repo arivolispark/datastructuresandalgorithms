dad3343e1511a956eaae66890d68c7ba37530096
arivolispark <arivolispark@gmail.com>
2025-01-13 21:35:34 -0800

    Update 2657_find_the_prefix_common_array_of_two_arrays.py

--- leetcode/leetcode_question_bank/problems/2657_find_the_prefix_common_array_of_two_arrays/2657_find_the_prefix_common_array_of_two_arrays.py
+++ leetcode/leetcode_question_bank/problems/2657_find_the_prefix_common_array_of_two_arrays/2657_find_the_prefix_common_array_of_two_arrays.py
@@ -1,1 +1,16 @@
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        result = []
+        map_1, map_2 = {}, {}
 
+        for i in range(len(A)):
+            map_1[A[i]] = A[i]
+            map_2[B[i]] = B[i]
+
+            match = 0
+            for k, v in map_1.items():
+                if k in map_2:
+                    match += 1
+            result.append(match)
+
+        return result
