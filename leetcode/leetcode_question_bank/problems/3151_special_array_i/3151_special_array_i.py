7c6c7186d29b8062fdac362a89aad86abc05f41e
arivolispark <arivolispark@gmail.com>
2025-01-31 21:55:32 -0800

    Update 3151_special_array_i.py

--- leetcode/leetcode_question_bank/problems/3151_special_array_i/3151_special_array_i.py
+++ leetcode/leetcode_question_bank/problems/3151_special_array_i/3151_special_array_i.py
@@ -1,1 +1,15 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        length = len(nums)
+        if length == 1:
+            return True
 
+        for i in range(1, length):
+            if nums[i - 1] % 2 == 0:
+                if nums[i] % 2 == 0:
+                    return False
+            else:
+                if nums[i] % 2 == 1:
+                    return False
+
+        return True
