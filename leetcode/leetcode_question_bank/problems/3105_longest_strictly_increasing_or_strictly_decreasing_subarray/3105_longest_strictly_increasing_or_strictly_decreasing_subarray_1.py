8f745c32b6dfc9f789200e8db07623b8c34e5960
arivolispark <arivolispark@gmail.com>
2025-02-02 19:55:05 -0800

    Update 3105_longest_strictly_increasing_or_strictly_decreasing_subarray_1.py

--- leetcode/leetcode_question_bank/problems/3105_longest_strictly_increasing_or_strictly_decreasing_subarray/3105_longest_strictly_increasing_or_strictly_decreasing_subarray_1.py
+++ leetcode/leetcode_question_bank/problems/3105_longest_strictly_increasing_or_strictly_decreasing_subarray/3105_longest_strictly_increasing_or_strictly_decreasing_subarray_1.py
@@ -1,1 +1,21 @@
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        length = len(nums)
+        increasing_length, decreasing_length = -math.inf, -math.inf
+        increasing_list, decreasing_list = [], []
+        
+        for i in range(length):
+            increasing_list.append(1)
+            decreasing_list.append(1)
 
+        for i in range(1, length):
+            if nums[i] > nums[i - 1]:
+                increasing_list[i] = increasing_list[i - 1] + 1
+        increasing_length = max(increasing_list)
+
+        for i in range(1, length):
+            if nums[i] < nums[i - 1]:
+                decreasing_list[i] = decreasing_list[i - 1] + 1
+        decreasing_length = max(decreasing_list)
+
+        return max(increasing_length, decreasing_length)
