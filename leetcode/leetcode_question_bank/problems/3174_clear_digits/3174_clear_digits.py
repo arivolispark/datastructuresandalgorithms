1cba2e6ffaf8ff1c20e3c4903755123f7d24698d
arivolispark <arivolispark@gmail.com>
2025-02-09 22:28:39 -0800

    Update 3174_clear_digits.py

--- leetcode/leetcode_question_bank/problems/3174_clear_digits/3174_clear_digits.py
+++ leetcode/leetcode_question_bank/problems/3174_clear_digits/3174_clear_digits.py
@@ -1,1 +1,10 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stack = []
+        for i in range(len(s)):
+            if (ord(s[i]) >= 48 and ord(s[i]) <= 57) and len(stack) > 0:
+                stack.pop()
+            else:
+                stack.append(s[i])
 
+        return "".join(stack)
