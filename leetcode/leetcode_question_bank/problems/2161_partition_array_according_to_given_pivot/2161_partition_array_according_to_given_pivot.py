4b49160c96e78bd650b87608682b42cfcf21dbb8
arivolispark <arivolispark@gmail.com>
2025-03-02 17:55:09 -0800

    Update 2161_partition_array_according_to_given_pivot.py

--- leetcode/leetcode_question_bank/problems/2161_partition_array_according_to_given_pivot/2161_partition_array_according_to_given_pivot.py
+++ leetcode/leetcode_question_bank/problems/2161_partition_array_according_to_given_pivot/2161_partition_array_according_to_given_pivot.py
@@ -1,1 +1,13 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        less, same, more = [], [], []
 
+        for i in range(len(nums)):
+            if nums[i] < pivot:
+                less.append(nums[i])
+            elif nums[i] == pivot:
+                same.append(nums[i])
+            else:
+                more.append(nums[i])
+
+        return less + same + more
