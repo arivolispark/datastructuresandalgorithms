88135a567e24119475979409d78a0210158b0353
arivolispark <arivolispark@gmail.com>
2025-03-02 17:36:52 -0800

    Update 2570_merge_two_2d_arrays_by_summing_values_2.py

--- leetcode/leetcode_question_bank/problems/2570_merge_two_2d_arrays_by_summing_values/2570_merge_two_2d_arrays_by_summing_values_2.py
+++ leetcode/leetcode_question_bank/problems/2570_merge_two_2d_arrays_by_summing_values/2570_merge_two_2d_arrays_by_summing_values_2.py
@@ -1,1 +1,119 @@
+"""
+Title:  2570. Merge Two 2D Arrays by Summing Values
 
+You are given two 2D integer arrays nums1 and nums2.
+
+nums1[i] = [idi, vali] indicate that the number with the id idi has a value equal to vali.
+nums2[i] = [idi, vali] indicate that the number with the id idi has a value equal to vali.
+Each array contains unique ids and is sorted in ascending order by id.
+
+Merge the two arrays into one array that is sorted in ascending order by id, respecting the following conditions:
+
+Only ids that appear in at least one of the two arrays should be included in the resulting array.
+Each id should be included only once and its value should be the sum of the values of this id in the two arrays. If the id does not exist in one of the two arrays, then assume its value in that array to be 0.
+Return the resulting array. The returned array must be sorted in ascending order by id.
+
+
+
+Example 1:
+Input: nums1 = [[1,2],[2,3],[4,5]], nums2 = [[1,4],[3,2],[4,1]]
+Output: [[1,6],[2,3],[3,2],[4,6]]
+Explanation: The resulting array contains the following:
+- id = 1, the value of this id is 2 + 4 = 6.
+- id = 2, the value of this id is 3.
+- id = 3, the value of this id is 2.
+- id = 4, the value of this id is 5 + 1 = 6.
+
+
+Example 2:
+Input: nums1 = [[2,4],[3,6],[5,5]], nums2 = [[1,3],[4,3]]
+Output: [[1,3],[2,4],[3,6],[4,3],[5,5]]
+Explanation: There are no common ids, so we just include each id with its value in the resulting list.
+
+
+
+Constraints:
+
+1) 1 <= nums1.length, nums2.length <= 200
+2) nums1[i].length == nums2[j].length == 2
+3) 1 <= idi, vali <= 1000
+4) Both arrays contain unique ids.
+5) Both arrays are in strictly ascending order by id.
+
+"""
+
+from typing import List
+
+
+class Solution:
+
+    def mergeArrays_1(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        result = []
+        map = {}
+
+        for i in range(len(nums1)):
+            start, end = nums1[i]
+            if start not in map:
+                map[start] = end
+            else:
+                v = map[start]
+                map[start] = v + end
+
+        for i in range(len(nums2)):
+            start, end = nums2[i]
+            if start not in map:
+                map[start] = end
+            else:
+                v = map[start]
+                map[start] = v + end
+
+        keys = []
+        for k, v in map.items():
+            keys.append(k)
+        keys.sort()
+
+        for i in range(len(keys)):
+            result.append([keys[i], map[keys[i]]])
+
+        return result
+
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        result = []
+        map = {}
+
+        helper(nums1, map)
+        helper(nums2, map)
+
+        keys = []
+        for k, v in map.items():
+            keys.append(k)
+        keys.sort()
+
+        for i in range(len(keys)):
+            result.append([keys[i], map[keys[i]]])
+
+        return result
+
+def helper(nums: List[List[int]], map: dict):
+    for i in range(len(nums)):
+        start, end = nums[i]
+        if start not in map:
+            map[start] = end
+        else:
+            v = map[start]
+            map[start] = v + end
+
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    test(solution.mergeArrays([[1,2],[2,3],[4,5]], [[1,4],[3,2],[4,1]]), [[1,6],[2,3],[3,2],[4,6]])
+    test(solution.mergeArrays([[2,4],[3,6],[5,5]], [[1,3],[4,3]]), [[1,3],[2,4],[3,6],[4,3],[5,5]])
