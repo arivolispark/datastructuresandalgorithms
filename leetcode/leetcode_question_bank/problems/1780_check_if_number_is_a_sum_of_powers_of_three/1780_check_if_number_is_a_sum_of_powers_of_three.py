3030699ef6f3675f18ab5b4e5a5236b93c89a948
arivolispark <arivolispark@gmail.com>
2025-03-03 23:58:53 -0800

    Update 1780_check_if_number_is_a_sum_of_powers_of_three.py

--- leetcode/leetcode_question_bank/problems/1780_check_if_number_is_a_sum_of_powers_of_three/1780_check_if_number_is_a_sum_of_powers_of_three.py
+++ leetcode/leetcode_question_bank/problems/1780_check_if_number_is_a_sum_of_powers_of_three/1780_check_if_number_is_a_sum_of_powers_of_three.py
@@ -1,1 +1,59 @@
+"""
+Title:  1780. Check if Number is a Sum of Powers of Three
 
+Given an integer n, return true if it is possible to represent n as the sum of
+distinct powers of three. Otherwise, return false.
+
+An integer y is a power of three if there exists an integer x such that y == 3x.
+
+
+
+Example 1:
+Input: n = 12
+Output: true
+Explanation: 12 = 31 + 32
+
+
+Example 2:
+Input: n = 91
+Output: true
+Explanation: 91 = 30 + 32 + 34
+
+
+Example 3:
+Input: n = 21
+Output: false
+
+
+Constraints:
+1) 1 <= n <= 10^7
+
+"""
+
+from typing import List
+
+
+class Solution:
+
+    def checkPowersOfThree(self, n: int) -> bool:
+        while n > 1:
+            n, r = divmod(n, 3)
+            if r == 2:
+                return False
+        return True
+
+
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    test(solution.checkPowersOfThree(12), True)
+    test(solution.checkPowersOfThree(91), True)
+    test(solution.checkPowersOfThree(21), False)
