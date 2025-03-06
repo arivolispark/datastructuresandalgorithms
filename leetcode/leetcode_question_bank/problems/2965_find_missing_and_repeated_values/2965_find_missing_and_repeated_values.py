d49dce6e70bbdb5afeb331886bdf2d295e0fd596
arivolispark <arivolispark@gmail.com>
2025-03-05 20:09:54 -0800

    Update 2965_find_missing_and_repeated_values.py

--- leetcode/leetcode_question_bank/problems/2965_find_missing_and_repeated_values/2965_find_missing_and_repeated_values.py
+++ leetcode/leetcode_question_bank/problems/2965_find_missing_and_repeated_values/2965_find_missing_and_repeated_values.py
@@ -1,1 +1,19 @@
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        m = len(grid)
+        n = len(grid[0])
+        map = {}
+        duplicate, missing = 0, 0
 
+        for i in range(m):
+            for j in range(n):
+                map[grid[i][j]] = map.get(grid[i][j], 0) + 1
+
+        for i in range(1, (m * n) + 1):
+            if i not in map:
+                missing = i
+            else:
+                if map[i] == 2:
+                    duplicate = i
+
+        return [duplicate, missing]
