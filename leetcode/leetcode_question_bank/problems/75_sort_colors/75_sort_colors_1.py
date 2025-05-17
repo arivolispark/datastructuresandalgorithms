bf50c7db52eb3ae847981dac8936f975fe10e8a8
arivolispark <arivolispark@gmail.com>
2025-05-16 22:52:00 -0700

    Update 75_sort_colors_1.py

--- leetcode/leetcode_question_bank/problems/75_sort_colors/75_sort_colors_1.py
+++ leetcode/leetcode_question_bank/problems/75_sort_colors/75_sort_colors_1.py
@@ -1,1 +1,25 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
 
+        map = {}
+        for i in range(len(nums)):
+            map[nums[i]] = map.get(nums[i], 0) + 1
+
+        r, w, b = 0, 0, 0
+
+        if 0 in map:
+            r = map[0]
+        if 1 in map:
+            w = map[1]
+        if 2 in map:
+            b = map[2]
+
+        for i in range(r):
+            nums[i] = 0
+        for i in range(r, r + w):
+            nums[i] = 1
+        for i in range(r + w, r + w + b):
+            nums[i] = 2
