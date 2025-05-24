06512d60f42f82283448ececd10847aa708a5d7d
arivolispark <arivolispark@gmail.com>
2025-05-23 23:35:08 -0700

    Update 2942_find_words_containing_character.py

--- leetcode/leetcode_question_bank/problems/2942_find_words_containing_character/2942_find_words_containing_character.py
+++ leetcode/leetcode_question_bank/problems/2942_find_words_containing_character/2942_find_words_containing_character.py
@@ -1,1 +1,7 @@
-
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        result = []
+        for i in range(len(words)):
+            if words[i].find(x) >= 0:
+                result.append(i)
+        return result
